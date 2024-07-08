c1861028bb51880bb0c483e534a1bcce690bd226
saul <sacorvacho@hotmail.com>
2024-07-08 14:40:12 -0400

    Segundo

---
+++ src/components/MyPlaylist.jsx
@@ -0,0 +1,24 @@
+import {SpotifyPlayer} from "./SpotityPlayer"
+
+export const MyPlaylist = () => {
+    return (
+    <div className='container'>
+        <h1 className='title text-center mt-5'>My favorite songs</h1>
+        <div className='d-flex align-items-center'>
+            <input type='text' className='form-control' placeHolder='Ingrese el codigo 
+    del album aqui...'></input>
+    <div className='form-check ms-2'>
+        <input className='form-check-input' type='checkbox' />
+        <label className='form-check-label'>
+            Favorite
+        </label>
+    </div>
+    <button className='btn btn-success ms-2'><i class="bi bi-plus-circle-fill"></i></button>
+    <button className='btn btn-danger ms-2'><i class="bi bi-trash"></i></button>
+    </div>
+        <div>
+            <SpotifyPlayer url="" favorite= {true}/>
+        </div>
+    </div>
+    )
+}
